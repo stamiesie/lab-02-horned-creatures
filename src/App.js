80c4c2a0ddbed358886c28fd5c273b2ba675fae6
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-02-09 18:24:41 -0800

    map, filter and render creatures

--- src/App.js
+++ src/App.js
@@ -1,3 +1,67 @@
 
+import React, { Component } from 'react'
 import './App.css';
+import ImageList from './image-list.js';
+import images from './data.js';
+import Header from './header.js';
+
+export default class App extends Component {
+    state = {
+        keyword: '',
+        horns: '',
+        title: ''
+    }
+
+    handleNameChange = (e) => {
+        this.setState({
+            keyword:
+                e.target.value
+        });
+
+    }
+
+    render() {
+        // filter
+        const filteredAnimals = images.filter((image) => {
+            console.log(this.state.keyword, image.keyword);
+            if (this.state.keyword === image.keyword) return true;
+
+            return false;
+
+        });
+        return (
+            <div className="main-page">
+                <Header />
+                <div className="select-menu">
+                    <select value={this.state.keyword}
+                        onChange={(e) => {
+                            this.setState({
+                                keyword: e.target.value
+                            })
+
+                        }}>
+                        <option value="">Creatures</option>
+                        <option value="narwhal">Narwhal</option>
+                        <option value="rhino">Rhino</option>
+                        <option value="unicorn">Unicorn</option>
+                        <option value="unilego">UniLego</option>
+                        <option value="triceratops">Triceratops</option>
+                        <option value="markhor">Markhor</option>
+                        <option value="mouflon">Mouflon</option>
+                        <option value="addax">Addax</option>
+                        <option value="chameleon">Chameleon</option>
+                        <option value="lizard">Lizard</option>
+                        <option value="dragon">Dragon</option>
+                    </select>
+
+                    <div className="photo-div">
+                        <ImageList images={filteredAnimals} />
+                    </div>
+                </div>
+            </div>
+
+
+        )
+    }
+}
 
---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Horned Creatures Gallery</h1>
+                <h3>Select Your Creature from the Menu Below</h3>
+            </div>
+        )
+    }
+}
